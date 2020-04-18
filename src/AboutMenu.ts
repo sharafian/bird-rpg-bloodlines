b4b2e999e8b951ea4eec3e3579e17621f4073dc9
sharafian <ben@sharafian.me>
2020-04-17 22:38:15 -0700

    feat: add menus

---
+++ src/AboutMenu.ts
@@ -0,0 +1,40 @@
+import Phaser from 'phaser'
+
+const AboutText = `
+Here is some text about the game and how
+to play it.
+
+Use the arrow keys to move, and hit up to
+fly. You need to find your mate to keep
+your bloodline alive
+
+Hit ENTER to return to the main menu`
+
+export class AboutMenu extends Phaser.Scene {
+  private menuY = 150
+
+  constructor () {
+    super('about-menu')
+  }
+
+  create () {
+    const headerStyle = {
+      color: 'white',
+      fontSize: '48px'
+    }
+
+    const itemStyle = {
+      color: 'white',
+      fontSize: '24px'
+    }
+
+    this.add.text(100, 100, 'About', headerStyle)
+    this.add.text(100, this.menuY, AboutText, itemStyle)
+
+    const enterKey = this.input.keyboard.addKey('ENTER')
+
+    enterKey.on('up', () => {
+      this.scene.start('main-menu')
+    })
+  }
+}
---
+++ src/MainMenu.ts
@@ -0,0 +1,65 @@
+import Phaser from 'phaser'
+
+export class MainMenu extends Phaser.Scene {
+  private selection = 0
+
+  private menuY = 250
+  private menuRowHeight = 30
+  private menuItems = [
+    { title: 'Start Campaign', scene: 'game-scene' },
+    { title: 'About', scene: 'about-menu' }
+  ]
+
+  constructor () {
+    super('main-menu')
+  }
+
+  create () {
+    const headerStyle = {
+      color: 'white',
+      fontSize: '48px'
+    }
+
+    const itemStyle = {
+      color: 'white',
+      fontSize: '24px'
+    }
+
+    this.add.text(100, 100, 'Bird RPG: Bloodlines', headerStyle)
+
+    for (let i = 0; i < this.menuItems.length; ++i) {
+      const item = this.menuItems[i]
+      this.add.text(100, this.menuY + i * this.menuRowHeight, item.title, itemStyle)
+    }
+
+    const cursors = this.input.keyboard.createCursorKeys()
+    const selectangle = this.add.rectangle(
+      250,
+      this.menuY + this.selection * this.menuRowHeight + 15,
+      300,
+      27,
+      0xffffff,
+      0.5
+    )
+
+    const updateSelect = () => {
+      selectangle.setY(this.menuY + this.selection * this.menuRowHeight + 15)
+    }
+
+    cursors.up?.on('down', () => {
+      this.selection = Math.max(this.selection - 1, 0)
+      updateSelect()
+    })
+
+    cursors.down?.on('down', () => {
+      this.selection = Math.min(this.selection + 1, 1)
+      updateSelect()
+    })
+
+    const enterKey = this.input.keyboard.addKey('ENTER')
+
+    enterKey.on('up', () => {
+      this.scene.start(this.menuItems[this.selection].scene)
+    })
+  }
+}
