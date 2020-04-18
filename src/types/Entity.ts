eea239560bc08850ee872b8763fdd54a68906562
sharafian <ben@sharafian.me>
2020-04-18 16:12:28 -0700

    feat: collide entities with world

--- src/types/Entity.ts
+++ src/types/Entity.ts
@@ -1,6 +1,9 @@
+import Phaser from 'phaser'
+
 export interface Entity {
   preload (): void
   create (): void
   update (): void
   getPosition (): void
+  getSprite (): Phaser.Physics.Arcade.Sprite
 }
