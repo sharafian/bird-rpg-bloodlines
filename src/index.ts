3bfff98ff1799c8a51b1a34ea29ed2a4eff5eac1
sharafian <ben@sharafian.me>
2020-04-18 19:58:27 -0700

    fix: turn off debug

--- src/index.ts
+++ src/index.ts
@@ -26,7 +26,7 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 500 },
-      debug: true
+      debug: false
     }
   },
   scene: [ MainMenu, GameScene, AboutMenu, MateScene ]
