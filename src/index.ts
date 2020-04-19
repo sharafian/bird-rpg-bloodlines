a1faddd2da375ade565c6b9b9e5d962306886485
Ben Sharafian <ben@sharafian.me>
2020-04-18 19:59:03 -0700

    feat: 'mating' screen between generations (#18)
    * style: lint
    
    * feat(wip): add roulette wheel for attributes
    
    * feat(wip): hatch egg
    
    * feat: connect scenes together
    
    * fix: turn off debug

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,14 @@ import Phaser from 'phaser'
 import { GameScene } from './GameScene'
 import { AboutMenu } from './AboutMenu'
 import { MainMenu } from './MainMenu'
+import { MateScene } from './MateScene'
 
 const ZOOM = 1
 
 const config = {
   type: Phaser.AUTO,
   pixelArt: true,
-  
+
   scale: {
     mode: Phaser.Scale.NONE,
     autoCenter: Phaser.Scale.CENTER_BOTH,
@@ -28,7 +29,7 @@ const config = {
       debug: false
     }
   },
-  scene: [ MainMenu, GameScene, AboutMenu ]
+  scene: [ MainMenu, GameScene, AboutMenu, MateScene ]
 }
 
 const game = new Phaser.Game(config)
